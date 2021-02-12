339798f39c202a53267e80c1093450389b1f44c5
Joey Whalen <jtktwhalen@gmail.com>
2021-02-11 19:20:25 -0500

    initial commit

---
+++ src/main/java/com/tts/introtospringbootttshw/IntroToSpringBootTtshwApplication.java
@@ -0,0 +1,13 @@
+package com.tts.introtospringbootttshw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToSpringBootTtshwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroToSpringBootTtshwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/introtospringbootttshw/IntroToSpringBootTtshwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.introtospringbootttshw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroToSpringBootTtshwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
