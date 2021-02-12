73613f452ba34753d19d8e02ad1395b8b73110ad
Joey Whalen <jtktwhalen@gmail.com>
2021-02-11 19:36:44 -0500

    update MainController to include String

--- src/main/java/com/tts/introtospringbootttshw/controller/MainController.java
+++ src/main/java/com/tts/introtospringbootttshw/controller/MainController.java
@@ -1,8 +1,13 @@
 package com.tts.introtospringbootttshw.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
+  @RequestMapping("/")
+  public String home() {
+	  return "My very own project";
+  }
 }
